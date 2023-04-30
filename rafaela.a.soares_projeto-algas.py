abef5bcab537ebfefc40bd5172daf617f9b6457f
RafaAmancioSoares <rafaela.soares@bandtec.com>
2023-04-30 16:27:15 -0300

    Atualizando projeto algas

--- rafaela.a.soares_projeto-algas.py
+++ rafaela.a.soares_projeto-algas.py
@@ -8,86 +8,85 @@ import random
 print("------------------------------------------------------------------------------------------------------------\n" +
       "PROJETO ALGAS -- RAFAELA AMANCIO SOARES -- GRUPO 3\n\n" +
       "Projeto: Analisar os dados de saúde para seguradoras/convênios através dos dados gerados pelo smart watch\n\n" +
-      "Análise - Sample 2\n"
+      "Análise - Sample\n"
       "------------------------------------------------------------------------------------------------------------")
 
-sample2 = range(1, 1441) # 1 usuário
+sample = range(1, 1441) # 1 usuário
 dataList = []
 
-for n in sample2:
+for n in sample:
     dataList.append(random.randint(0, 191))
 print("Criando lista com valores de entrada\n\n")
 
-# try:
-inicio = time.time()
-db_connection = mysql.connector.connect(
-    host='localhost', 
-    port='3306',
-    user="root",
-    password="urubu100",
-    database="algas_transactions")
-print("Iniciando conexão com database\n")
-
-cursor = db_connection.cursor()
-
-#     cursor.execute("TRUNCATE TABLE passos_adulto")
-print("Limpando histórico da tabela\n")
-
-
-print("Iniciando transações\n\n")
-print("---- Without Memoryview Sample 2 ----")
-vwWithoutMemorySample2 = []
-count = 0
-for n in sample2:
-    data = dataList[n-1]
-    b = data
-    start = time.time()
-    max_mem = 0
-    min_mem = 0
-    while b:
-        if n == len(str(b)):
-            max_mem = getsizeof(b) - getsizeof('')
-        elif len(str(b)) == 1:
-            min_mem = getsizeof(b) - getsizeof('')
-        b = str(b)[1:]
-    stop = time.time()
-    count += 1
+try:
+    inicio = time.time()
+    db_connection = mysql.connector.connect(
+        host="localhost", 
+        port="3306",
+        user="root",
+        password="urubu100",
+        database="algas_transactions")
+    print("Iniciando conexão com database\n")
     
-    final_time = stop-start
-    max_mem = max_mem/10**3
+    cursor = db_connection.cursor()
+    
+    # cursor.execute("TRUNCATE TABLE passos_adulto")
+    # print("Limpando histórico da tabela\n")
+
+    print("Iniciando transações\n\n")
+    print("---- Without Memoryview Sample ----")
+    vwWithoutMemorysample = []
+    count = 0
+    for n in sample:
+        data = dataList[n-1]
+        b = data
+        start = time.time()
+        max_mem = 0
+        min_mem = 0
+        while b:
+            if n == len(str(b)):
+                max_mem = getsizeof(b) - getsizeof('')
+            elif len(str(b)) == 1:
+                min_mem = getsizeof(b) - getsizeof('')
+            b = str(b)[1:]
+        stop = time.time()
+        count += 1
+        
+        final_time = stop-start
+        max_mem = max_mem/10**3
 #         print(f'{count}º Transação {n} {final_time} - Max mem {max_mem} KB - Min mem {min_mem} B')
-    vwWithoutMemorySample2.append(stop-start)
+        vwWithoutMemorysample.append(stop-start)
+        
+        query = "INSERT INTO passos_adulto(data) " \
+                "VALUES(%s)"
+        args = (data, )
     
-    query = "INSERT INTO passos_adulto(data) " \
-            "VALUES(%s)"
-    args = (data, )
-
-    cursor.execute(query, args)
-    db_connection.commit()
+        cursor.execute(query, args)
+        db_connection.commit()
+        
     
-
-print("\n\n---- With Memoryview Sample 2 ----")
-vwMemorySample2 = []
-count = 0
-for n in sample2:
-    data = b'dataList[n-1]'
-    b = memoryview(data)
-    start = time.time()
-    max_mem = 0
-    min_mem = 0
-    while b:
-        if n == len(str(b)):
-            max_mem = getsizeof(b) - getsizeof('')
-        elif len(str(b)) == 1:
-            min_mem = getsizeof(b) - getsizeof('')
-        b = str(b)[1:]
-    stop = time.time()
-    count += 1
-    
-    final_time = stop-start
-    max_memory = max_mem/10**3
+    print("\n\n---- With Memoryview Sample ----")
+    vwMemorysample = []
+    count = 0
+    for n in sample:
+        data = b'dataList[n-1]'
+        b = memoryview(data)
+        start = time.time()
+        max_mem = 0
+        min_mem = 0
+        while b:
+            if n == len(str(b)):
+                max_mem = getsizeof(b) - getsizeof('')
+            elif len(str(b)) == 1:
+                min_mem = getsizeof(b) - getsizeof('')
+            b = str(b)[1:]
+        stop = time.time()
+        count += 1
+        
+        final_time = stop-start
+        max_memory = max_mem/10**3
 #         print(f'{count}º Transação {n} {final_time} - Max mem {max_memory} KB - Min mem {min_mem} B')
-    vwMemorySample2.append(stop-start)
+        vwMemorysample.append(stop-start)
         
     final = time.time()
 
@@ -104,12 +103,12 @@ for n in sample2:
 
     # Plot everything
 #     import matplotlib.pyplot as plt
-#     plt.plot(vwWithoutMemorySample2, 'x-', label='Without Memoryview Sample 2')
-#     plt.plot(vwMemorySample2, 'o--', label='With Memoryview Sample 2')
+#     plt.plot(vwWithoutMemorysample, 'x-', label='Without Memoryview Sample')
+#     plt.plot(vwMemorysample, 'o--', label='With Memoryview Sample')
 #     plt.xlabel('Size of Bytearray')
 #     plt.ylabel('Time (s)')
 #     plt.legend()
 #     plt.show()
     
-# finally:
-#     db_connection.close()
+finally:
+    db_connection.close()
